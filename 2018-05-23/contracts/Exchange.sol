e38268edd8cdd3123bb62bf904a1262c478864a5
quantum13 <quant13@gmail.com>
2018-05-23 14:26:49 +0300

    atomic swap registry, skeleton for exchange, tests helpers

---
+++ 2018-05-23/contracts/Exchange.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.17;
+
+import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
+import './AtomicSwapRegistry.sol';
+
+
+contract Exchange {
+    using SafeMath for uint256;
+
+
+}
+
