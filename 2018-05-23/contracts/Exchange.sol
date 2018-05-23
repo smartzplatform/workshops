2113b7fc2cfc8ed8382a80842231adb869af0546
quantum13 <quant13@gmail.com>
2018-05-23 15:14:11 +0300

    deposits and withdrawals

--- 2018-05-23/contracts/Exchange.sol
+++ 2018-05-23/contracts/Exchange.sol
@@ -15,5 +15,26 @@ contract Exchange {
 
     AtomicSwapRegistry public swapRegistry;
 
+    mapping (address => uint) public deposits;
+
+
+    /*****************************************************************/
+
+    function myDeposit() view public returns (uint) {
+        return deposits[msg.sender];
+    }
+
+    function deposit() public payable {
+        deposits[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint _amount) public {
+        require(deposits[msg.sender] >= _amount);
+
+        deposits[msg.sender] -= _amount;
+
+        msg.sender.transfer(_amount);
+    }
+
 }
 
