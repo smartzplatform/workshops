acd9ed146de56d334de46b30877062c6fda6a30c
quantum13 <quant13@gmail.com>
2018-05-23 20:08:29 +0300

    interface for buy and sell

--- 2018-05-23/contracts/Exchange.sol
+++ 2018-05-23/contracts/Exchange.sol
@@ -13,11 +13,38 @@ contract Exchange {
 
     /*****************************************************************/
 
+    /**
+     * Blockchains to swap with (one of them with be useless, since exchange contract will be deployed to it)
+     */
+    uint8 constant ETH = 1;
+    uint8 constant ETH_KOVAN = 2;
+    uint8 constant ETH_RINKEBY = 3;
+    uint8 constant EOS = 4;
+    uint8 constant BITCOIN = 5;
+
+    /*****************************************************************/
+
     AtomicSwapRegistry public swapRegistry;
 
 
     mapping (address => uint) public deposits;
 
+
+    /*****************************************************************/
+
+    /**
+     * want to get some currency, give back ether
+     */
+    function buy(uint8 _secondBlockchain, uint _currencyCount, uint _priceInWeiForOneUnit) public {
+
+    }
+
+    /**
+     * want to get ether, give back some currency
+     */
+    function sell(uint8 _secondBlockchain, uint _currencyCount, uint _priceInWeiForOneUnit) public {
+
+    }
 
     /*****************************************************************/
 
