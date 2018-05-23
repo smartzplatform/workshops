f28e765d375c09e21a9ce4aeeac350a65d67718b
quantum13 <quant13@gmail.com>
2018-05-23 14:02:54 +0300

    init commit for workshoop 2018-05-23

---
+++ 2018-05-23/truffle.js
@@ -0,0 +1,22 @@
+module.exports = {
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 9545,
+            network_id: "*" // match any network
+        },
+        rinkeby: {
+            host: "127.0.0.1",
+            port: 8544,
+            network_id: 4,
+            gasPrice: 20*10**9,
+            // from: ''
+        }
+    },
+    solc: {
+        optimizer: {
+            enabled: true,
+            runs: 200
+        }
+    }
+};
